d7d3777dff7781e7187fb2e3f4228ef13e2ce1c4
swamini10 <swaminibhandare10@gmail.com>
2025-12-02 10:33:10 +0530

    login com

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Login],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ov_ui');
+ title : string = 'ov_ui';
 }
